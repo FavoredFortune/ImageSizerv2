05846df2670c360dbe9671cf9f7ab5246a2e67e6
Suzanne Richman <suzanne@favoredfortune.com>
2018-10-24 12:22:55 -0700

    working crop and resizing calls - simplified adding firebase dependency

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ As a photojournalist, I want to be able to upload my large format JPG images to 
 # Instructions for use/review
 - After cloning/forking the repository, please download all dependencies by typing npm i in the command line of your terminal interface within the project folder.
 - You can then launch the server back end in the terminal with the command 
-`node server.js` (this shows the server at localhost:3000)
+`node index.js` (this shows the server at localhost:3000)
 - To see the front end interface, from the same folder in the terminal type in live-server (this shows the front end at localhost:8080/)
 
 # Technology
@@ -22,22 +22,16 @@ As a photojournalist, I want to be able to upload my large format JPG images to 
  - Node.js (8 LTS)
  - Homebrew
  - Handlebars
- - Page.js
  - JQuery
  - Git
  - GitHub
  - Visual Studio Code
  - Dependencies:
     - Express
-    - Https
-    - Http
     - CORS
     - Body-parser
-    - Superagent
-    - Nodemon
-    - Firebase
-    - Live-server
     - GraphicsMagick
+    - FS (File Server)
 
 # Resources
 - Referenced my own project, [On This Day](On-This-Day/thisday-server)
--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const express = require ('express');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const https = require ('https');
-// const gm = require('graphicsmagick');
+const gm = require('graphicsmagick');
+const fs = require('fs');
+const firebase = require('firebase');
+
 
 //application setup
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//database set up
+const DATABASE_URL = 'https://console.firebase.google.com/u/1/project/photoapp-c643e/database/photoapp-c643e/data';
+
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyBb8tDj-oO_fBoGX1KP0MTDO4GRcv5KJm4',
+  authDomain: 'photoapp-c643e.firebaseapp.com',
+  databaseURL: 'https://photoapp-c643e.firebaseio.com',
+  projectId: 'photoapp-c643e',
+  storageBucket: 'photoapp-c643e.appspot.com',
+  messagingSenderId: '170890289058'
+};
+firebase.initializeApp(config);
 
 //for access to cross origin resource sharing
 app.use(cors());
@@ -21,4 +38,33 @@ app.get('/home', (request,response) => {
 
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`);
+});
+
+//request original image
+app.get(`${DATABASE_URL}/{rawImagePath}/rawImageName`, (request,response)=>{
+  gm(`${DATABASE_URL}/{rawImagePath}/rawImageName`)
+    .write('./tmp.jpg', (error)=>{
+      if(error){
+        console.log(error);
+      } else{
+        response.sendFile('./tmp.jpg');
+      }
+    });
+  //deletes temporary file downloaded
+  fs.unlinkSync('./tmp.jpg');
+});
+
+
+//request resize image
+app.get(`${DATABASE_URL}/{rawImagePath}/rawImageName`, (request,response)=>{
+  gm(`${DATABASE_URL}/{rawImagePath}/rawImageName`)
+    .write('./tmp.jpg', (error)=>{
+      if(error){
+        console.log(error);
+      } else{
+        response.sendFile('./tmp.jpg');
+      }
+    });
+  //deletes temporary file downloaded
+  fs.unlinkSync('./tmp.jpg');
 });
